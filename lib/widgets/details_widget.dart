7702b60490e25e950eff7407e1d3fad82403f9da
Sameh Mohamed <sameh.abdelhaleem95@gmail.com>
2021-04-02 19:20:15 +0200

    move details widget to it's own dart file

--- lib/widgets/details_widget.dart
+++ lib/widgets/details_widget.dart
@@ -1,88 +1,103 @@
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_provider/ui/post_details/post_details_screen.dart';
-//
-// class DetailsWidget extends StatelessWidget {
-//   final details;
-//
-//   const DetailsWidget({Key key, this.details}) : super(key: key);
-//   @override
-//   Widget build(BuildContext context) {
-//
-//       return  Padding(
-//         padding: const EdgeInsets.all(4.0),
-//         child: Card(
-//             shape: RoundedRectangleBorder(
-//                 borderRadius: BorderRadius.circular(12)),
-//             child: Row(
-//
-//               children: [
-//                 SizedBox(
-//                     height: 100, child: Image.network('https://image.tmdb.org/t/p/w500/${details.posterPath}')),
-//                 Padding(
-//                   padding: const EdgeInsets.only(left: 8),
-//                   child: Row(
-//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                     children:[
-//                       Column(
-//                         mainAxisAlignment: MainAxisAlignment.center,
-//                         crossAxisAlignment: CrossAxisAlignment.start,
-//                         children: [
-//                           Padding(
-//                             padding: const EdgeInsets.all(2.0),
-//                             child: Center(
-//                                 child: Text(
-//                                   details.title,
-//                                   textAlign: TextAlign.center,
-//                                   style: TextStyle(
-//                                       fontWeight: FontWeight.bold),
-//                                 )),
-//                           ),
-//                           Padding(
-//                             padding: const EdgeInsets.all(2.0),
-//                             child: Center(
-//                                 child: Text(
-//                                   'Release on: ${ details.releaseDate}',
-//                                   textAlign: TextAlign.center,
-//                                   style: TextStyle(
-//                                       color:
-//                                       CupertinoColors.inactiveGray),
-//                                 )),
-//                           ),
-//                           Padding(
-//                             padding: const EdgeInsets.all(2.0),
-//                             child: Center(
-//                                 child: Text(
-//                                   'Rating: ${ details.voteAverage}',
-//                                   textAlign: TextAlign.center,
-//                                   style: TextStyle(
-//                                       color:
-//                                       CupertinoColors.inactiveGray),
-//                                 )),
-//                           ),
-//                           Padding(
-//                             padding: const EdgeInsets.all(2.0),
-//                             child: Center(
-//                                 child: Text(
-//                                   'Vote Count: ${ details.voteCount}',
-//                                   textAlign: TextAlign.center,
-//                                   style: TextStyle(
-//                                       color:
-//                                       CupertinoColors.inactiveGray),
-//                                 )),
-//                           ), // getTextWidgets( post.results[index].title),
-//
-//                         ],
-//                       ),
-//
-//                     ],
-//
-//                   ),
-//                 ),
-//
-//               ],
-//             )),
-//       );
-//
-//   }
-// }
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:popular/ui/person_details/person_details_screen.dart';
+
+
+
+class DetailsWidget extends StatelessWidget {
+  final  details;
+  const DetailsWidget({Key key, this.details}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child:Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+              child: Column(
+
+                children: [
+
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children:[
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Center(
+                                  child: Text(
+                                    details.name,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold),
+                                  )),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 10,bottom: 10),
+                              child: Container(
+                                  width: MediaQuery.of(context).size.width * 0.8,
+                                  child: Text('biography: ${details.biography}')
+                              ),
+                            ),
+
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Center(
+                                  child: Text(
+                                    'birthday: ${ details.birthday}',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color:
+                                        CupertinoColors.inactiveGray),
+                                  )),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Center(
+                                  child: Text(
+                                    'Rating: ${ details.popularity}',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color:
+                                        CupertinoColors.inactiveGray),
+                                  )),
+                            ),
+
+
+                          ],
+                        ),
+
+                      ],
+
+                    ),
+                  ),
+
+                ],
+              )),
+        )); ;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
