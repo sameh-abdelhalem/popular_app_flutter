b6a91adaa972010a479c0d4464233b9c9e9ffb82
Sameh Mohamed <sameh.abdelhaleem95@gmail.com>
2021-04-02 18:37:22 +0200

    link details service to repo

--- lib/repositories/people_repository.dart
+++ lib/repositories/people_repository.dart
@@ -1,16 +1,18 @@
 
 
 
+import 'package:popular/models/details.dart';
 import 'package:popular/models/person.dart';
 import 'package:popular/services/people_list_service.dart';
+import 'package:popular/services/person_details_service.dart';
 
 class PeopleRepositry{
   PeopleListService _peopleListService = PeopleListService();
-  // DetailsService _detailsService =DetailsService();
+  DetailsService _detailsService =DetailsService();
   Future<Person> fetchPeople(){
     return _peopleListService.fetchPosts();
   }
-  // Future<Details> fetchDetails(movieId){
-  //   return _detailsService.fetchDetails(movieId);
-  // }
+  Future<Details> fetchDetails(personId){
+    return _detailsService.fetchDetails(personId);
+  }
 }
